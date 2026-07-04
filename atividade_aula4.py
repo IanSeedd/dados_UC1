dd9c22c72e12a83db8e7a08da66248effb070c1a
IanSeedd <ianseedwebdev@gmail.com>
2026-07-03 21:44:57 -0300

    Atividade de tupla e lista

--- atividade_aula4.py
+++ atividade_aula4.py
@@ -23,16 +23,18 @@
 
 # Cadastro: 
 lista = []
+# Anotação do futuro: O melhor jeito é usar mais de uma lista ai os index vão se alinhar logo é facil de printar também, então é só fazer uma lógca onde se a idade for menor que 18 ele cancela tudo antes do append
 for i in range(1, 11):
     nome = input("Digite seu nome: ")
     idade = int(input("Digite sua idade: "))
     telefone = input("Digite seu telefone: ")
     email = input("Digite seu email: ")
     formacao = input("Digite sua formação: ")
-    dado = f"Nome: {nome} | Idade: {idade} | Telefone = {telefone} | Email: {email} | Formação: {formacao}" # Cada dado em string pra não fazer uma lista dentro de uma lista
+    dado = f"Nome: {nome} | Idade: {idade} | Telefone = {telefone} | Email: {email} | Formação: {formacao}" # Cada dado em string 
     if idade >= 18:
         print("Seus dados foram adicionados")
         lista.append(dado)
     print("Candidato inválido, menor de 18")
 for i in range(len(lista)):
     print(f"Candidato {i+1}: \n{lista[i]}")
+# Eu corrigi já, ta no jupiter 
