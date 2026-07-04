6236eba4673a61dd9bb7bec50c9f0a53f1cda853
IanSeedd <ianseedwebdev@gmail.com>
2026-07-03 21:46:04 -0300

    Dicionario

---
+++ aula_five.py
@@ -0,0 +1,21 @@
+# aluno = {
+#     "nome": "Carlos",
+#     "idade": 20,
+#     "curso": "Analista de Dados",
+#     "ativo": True
+# }
+# print(aluno.get("cidade"))
+# print(aluno.get("curso"))
+# print(aluno["idade"])
+# aluno["cidade"] = "Xique-Xique"
+# print(aluno.get("cidade"))
+# aluno["idade"] = 900
+# print(aluno["idade"])
+# print("turma" in aluno)
+
+candidatos = {
+"nomes": ["Goku", "Vegeta", "Gohan"],
+"idades": [16, 18, 14],
+"cidades":["Niteroi", "Rio", "Sao Paulo"],
+"vagas": ["Analista", "Dev", "Cientista"]
+}
